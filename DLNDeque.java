e7c335fd58d0a9f5085e595e1803e06f2bf44a01
Terry Guan <terryguan513@gmail.com>
2017-04-03 10:17:58 -0400

    added methods to DLNQ

--- DLNDeque.java
+++ DLNDeque.java
@@ -1,26 +1,45 @@
 public class DLNDeque<T> implements Deque<T> {
+
+  private LLNode<T> _front, _end;
+  public int _size;
+
+  public DLNDeque() {
+    _front = _end = null;
+  }
+
+  public void addFirst(T e) {
+
+  }
+  public T removeFirst() {
     
-    private LLNode<T> _front;
-    private LLNode<T> _end;
-    public int _size;
+  }
 
-    public DLNDeque(){
-	_front = _end = null;
-    }
+  // returns true if the given argument is in the deque
+  public boolean contains(T e);
 
-    public boolean contains(T e){
-	
-    }
+  // adds value at end of deque represented by queue
+  public void addLast(T e) {
 
-    public T peekFirst(){
-	return _front.getValue();
-    }
+  }
 
-    public T peekLast(){
-	return _end.getValue();
-    }
+  // removes value at end of deque
+  public T removeLast() {
 
-    public int size(){
-	return _size;
-    }
+  }
+
+  public boolean contains(T e) {
+
+  }
+
+  public T peekFirst() {
+    return _front.getValue();
+  }
+
+  public T peekLast() {
+    return _end.getValue();
+  }
+
+  public int size(){
+    return _size;
+  }
 }
